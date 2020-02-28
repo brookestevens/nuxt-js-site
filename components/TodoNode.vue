1c1fad61974e99c94af22df64fbdffa7fb77d129
brookestevens <brst4163@colorado.edu>
2020-02-27 22:02:49 -0700

    updating card styles and UI

--- components/TodoNode.vue
+++ components/TodoNode.vue
@@ -1,33 +1,47 @@
 <template>
     <!-- The information and modal -->
-    <div class="todo-child">
-        <input class="todo-checkbox" v-model="checked" type="checkbox"/>
-        <h4 :class="isChecked" @click="handleClick($bvModal)">{{todo.name}}</h4>
-        <p :class="isChecked" >{{todo.desc}}</p>
-        <b-modal :id="'modal-'+todo.id" :title="todo.name" hide-footer>
-            <EditItemForm :todo="todo"/>
-        </b-modal>
+    <div :class="`todo-child ${level}`">
+        <b-card>
+            <b-card-text>
+                <b-form-input :id="`name-${todo.id}`" @blur="handleUpdate" class="edit-form-input" v-model="taskName" placeholder="Task name"></b-form-input>
+                <h6 @click="showTimerPage(todo)" class="work-on-task" > Work on this task! </h6>
+                <img @click="handleDelete" id="edit-icon" src="/images/trash-can.png" alt="kill me plz" />
+            </b-card-text>
+        </b-card>
     </div>
 </template>
 
 <script>
-import EditItemForm from "../components/EditItemForm";
 export default {
     name: "TodoNode",
     props: {
-        todo: Object
+        todo: Object,
+        level: String,
+        taskID: String
     },
     data: function(){
         return{
-            checked: false
+            checked: false,
+            taskName: this.todo.name
         }
     },
     methods:{
-        handleClick: function(m){
-            this.$store.dispatch('setCurrentItem', this.todo);
-            var modalID = 'modal-' + this.todo.id;
-            m.show(modalID);
-        }
+        showTimerPage: function(todo){
+            this.$store.dispatch('setCurrentItem', this.todo)
+            .then(() => this.$router.push('/timer'))
+            .catch(err => console.error(err));
+        },
+        handleDelete: function(){
+            //alert("deleting at index: " + this.todo.id);
+            this.$store.dispatch('delete', {id: this.todo.id, taskID: this.taskID})
+            .then(() => this.$router.push('/dashboard'))
+            .catch(err => console.error(err));
+        },
+        handleUpdate: function(){
+            //alert(`Updating the item: ${this.taskName}`); //works!
+          this.$store.dispatch('update', {form: {...this.todo, name: this.taskName}, taskID: this.taskID})
+          .catch(err => console.error(err));
+        },
     },
     computed: {
         isChecked(){
@@ -38,37 +52,58 @@ export default {
                 return "task-unchecked";
             }
         }
-    },
-    components:{
-        EditItemForm
     }
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
     .todo-container{
         display: flex;
-    }
-    .todo-child{
-        margin-left: 1.5em;
     }
     .todo-checkbox{
         display: inline-block;
     }
     h4{
         display: inline-block;
-    }
-    h4:hover{
-        color: red;
-    }
-    p{
-        text-align: left;
+        margin-bottom: 0px;
     }
     .task-checked{
         text-decoration: line-through;
     }
     .task-unchecked{
         text-decoration: none;
+    }
+    .nibble{
+        margin-left: 2em;
+    }
+    #edit-icon{
+        height: 25px;
+        width: auto;
+        float: right;
+    }
+    #edit-icon:hover{
+        cursor: pointer;
+    }
+    .work-on-task{
+        margin-bottom: 0px;
+        color: darkgray;
+        max-width: 140px;
+        text-decoration: underline;
+    }
+    .work-on-task:hover{
+        color: $peach;
+        cursor: pointer;
+    }
+    .edit-form-input{
+        border: none;
+        padding-left: 0px;
+        margin-bottom: .5em;
+        font-size: 1.25em;
+        font-weight: 500;
+    }
+    .edit-form-input:hover{
+        border-bottom: 1px solid black;
+        cursor: pointer;
     }
 
 </style>
--- components/TodoTree.vue
+++ components/TodoTree.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
     <!-- Actual content of the todo -->
-    <TodoNode :todo="todo"/>
+    <TodoNode :taskID="taskID" :level="bite" :todo="todo"/>
     <div :id=" childrenId ">
     <template v-if="todo.children.length > 0">
         <!-- Render any Sub Tasks -->
         <template v-for="i in todo.children">
-          <TodoTree :key="i.id" :todo="i" />
+          <!-- Replace with todoTree for n deep layers of children. Doing this for styling sake -->
+          <TodoNode :taskID="taskID" :level="nibble" :key="i.id" :todo="i" />
         </template>
     </template>
     </div>
@@ -17,9 +18,16 @@
 import TodoNode from "../components/TodoNode";
   export default{
     name: "TodoTree",
+    data: function(){
+      return{
+        bite: 'bite',
+        nibble: 'nibble'
+      }
+    },
     props:{
       todo: Object,
-      childrenId: String
+      childrenId: String,
+      taskID: String //Always reference to the task ID
     },
     components: {TodoNode}
   }
