75f39b4b771ecb8242c0f7a1b4217c9523b155c2
brookestevens <brst4163@colorado.edu>
2020-02-27 22:03:42 -0700

    set local storage values

---
+++ plugins/localStorage.js
@@ -0,0 +1,6 @@
+//Set local storage once
+
+localStorage.setItem('sort', 'priority');
+localStorage.setItem('dark', 'false');
+localStorage.setItem('login', 'false');
+localStorage.setItem('taskInProgress', 'false');
