90e372bdb2e4a6e07cd67c54096fc7d0e80a325e
brookestevens <brst4163@colorado.edu>
2020-02-27 22:01:25 -0700

    fixing recursive update and delete

--- store/index.js
+++ store/index.js
@@ -1,8 +1,11 @@
 import todosDb from '../assets/openIndexDB';
+const check = String.fromCharCode(9989);
+const cancel = String.fromCharCode(0x274C);
 
 export const state = () => ({
   todos: [], //indexDB
-  currentToDo: {}, //indexDB
+  currentToDo: null, //indexDB
+  taskInProgress: 'false',
   loggedIn: 'false', //localstorage
   sort: 'all', //localstorage
   dark: 'false' //localstorage
@@ -38,21 +41,24 @@ export const mutations = {
   },
   DARK_MODE(state) {
     state.dark = localStorage.getItem('dark');
+  },
+  TOGGLE_TASK(state){
+    state.taskInProgress = localStorage.getItem('taskInProgress');
   }
 }
 
 export const actions = {
   addTodo({ commit }, payload) {
-    //format payload to what indexed db wants
-    let f = { ...payload, id: "tempID-" + Math.floor(Math.random() * 100000), status: false }; //add these 2 props for IDB
-    f = pgToIDB(f); //if the added task has sub tasks, then children need to be formatted
-    console.log(f);
+    // format payload to what indexed db wants
+    let format = pgToIDB(payload);
+    console.log('Payload: ', payload);
+    //console.log('Formatting', pgToIDB(payload)); //if the added task has sub tasks, then children need to be formatted
     todosDb.connect()
       .then(db => {
         //add to indexed db with response
         const tx = db.transaction('all-todos', 'readwrite');
-        tx.store.add(f)
-        .then(() => commit('ADD', f))
+        tx.store.add(format)
+        .then(() => commit('ADD', format))
         .then(() => tx.done)
         .then(() =>{
           fetch('/api/addTask', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(payload) })
@@ -60,24 +66,58 @@ export const actions = {
         })
       }).catch(err => console.error(err)); //idb not available
   },
-  addNibble({ commit }, payload) {
-    console.log("adding nibble: ", payload);
-    return;
+  addSingleTask({commit}, payload){
+      todosDb.connect()
+      .then(db => {
+        //add to indexed db with response
+        const tx = db.transaction('all-todos', 'readwrite');
+        // need to format this for IDB
+        let f = {...payload, id: "tempID-" + Math.floor(Math.random() * 100000), status: false, children: []}
+        tx.store.add(f)
+        .then(() => commit('ADD', f))
+        .then(() => tx.done)
+        .then(() =>{
+          fetch('/api/addSingleTask', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(payload) })
+          .catch(err => console.error("Error adding to PG: ", err))
+        })
+      }).catch(err => console.error(err)); //idb not available
   },
+  //want to update the task index and delete
   delete({ commit }, payload) {
-    let deletedList = deleteTask(this.state.todos, payload);
-    todosDb.connect().then(db => {
-        db.delete('all-todos', payload.id)
-          .then(() => {
+    return todosDb.connect().then(db => {
+        const tx = db.transaction('all-todos', 'readwrite');
+        tx.objectStore('all-todos').getAll()
+        .then(results => {
+          deleteTask(results, payload);
+          commit('DELETE', results); //REPLACE THE WHOLE ARRAY
+          console.log(check + " Deleted array: ", results );
+          let result = results.filter(i => i.id === payload.taskID);
+          return {
+            idToDelete: payload.id,
+            taskID: payload.taskID,
+            index: result[0] //for replacement
+          };
+        })
+        .then( ids => {
+          if(ids.idToDelete.substring(0,4) === 'task' ){
+            console.log("DELETE THE WHOLE INDEX");
+            tx.store.delete(ids.taskID);
+          }
+          else{
+            console.log("updating the index instead with: ", ids.index);
+            tx.store.put(ids.index);
+          }
+        })
+        .then(() => tx.done )
+        .then(() => {
             fetch('/api/delete', {method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({id: payload.id})})
             .catch(err => console.error("Offline, or deletion error in PG: ", err))
-          })
-          .then(() => commit('DELETE', deletedList));
+        })
       })
       .catch(err => console.error("Error deleting: ", err));
   },
   setCurrentItem({ commit }, payload) {
-    todosDb.connect().then(db => {
+    return todosDb.connect().then(db => {
       db.add('curr-todo', payload)
         .catch(err => db.put('curr-todo', payload))
     })
@@ -85,27 +125,38 @@ export const actions = {
       .catch(err => console.error('couldnt set todo: ', err));
   },
   update({ commit }, payload) {
-    let updatedList = updateTask(this.state.todos, payload);
     todosDb.connect().then(db => {
-      db.put('all-todos', payload)
-      .then(() => {
-        fetch('/api/updateTask', {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(payload)})
-        .then(res => res.json()).catch(err => console.log("Offline or PG update error: ", err)); //network error, background sync
+      const tx = db.transaction('all-todos', 'readwrite');
+      tx.objectStore('all-todos').getAll()
+      .then(results => {
+        updateTask(results, payload.form);
+        commit('UPDATE', results); //REPLACE THE WHOLE ARRAY
+        console.log(check + " UPDATED ARRAY: ", results);
+        console.log(check + " updating this index: ", payload.taskID);
+        //need to find the updated index
+        let result = results.filter(i => i.id === payload.taskID);
+        return result[0]; // only 1 index because the index is always UNIQUE
       })
+      .then(result => tx.store.put(result))
+      .then(() => tx.done)
       .then(() => {
-        commit('UPDATE', updatedList);
-      });
+        // SEND TO SERVER 
+        fetch('/api/updateTask', {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(payload)})
+        .then(res => res.json())
+        .catch(err => console.log("Offline or PG update error: ", err)); //network error, background sync
+      })
     }).catch(err => console.error('couldnt set todo: ', err));
   },
   getAllTodos({ commit }) {
     fetch('/api/getEverything') //this route is intercepted by SW
       .then(res => res.json())
       .then(res => {
+        console.log(check + " Server response: ", res.results);
         commit('GET_ALL_TODOS', res.results); //set state to either server / IDB response
         return res;
       })
       .then(res => {
-        if (res.status === 1) { //if the response came back from the network, update state
+        if (res.responseCode === 1) {
           todosDb.connect().then(db => {
             const tx = db.transaction('all-todos', 'readwrite');
             tx.objectStore('all-todos').clear().then(() => {
@@ -118,7 +169,7 @@ export const actions = {
           })
           .catch(err => console.error("Error adding all todos to IDB: ", err));
         }
-        else if(res.status === 2){
+        else if(res.responseCode === 2){
           // no need to update IDB
           console.log("Data came from IDB, not updating");
         }
@@ -169,6 +220,11 @@ export const actions = {
   logout({ commit }) {
     commit('LOGOUT');
   },
+  toggleTask({commit}){
+    let s = !JSON.parse(this.state.taskInProgress);
+    localStorage.setItem('taskInProgress', s.toString());
+    commit('TOGGLE_TASK');
+  },
   changeView({ commit }) {
     let s = !JSON.parse(this.state.dark);
     localStorage.setItem('dark', s.toString());
@@ -177,48 +233,85 @@ export const actions = {
 
 }
 
-//format the data's children array
+//format the data for IDB, also how PG returns data to client
 function pgToIDB(payload) {
-  let copy = { ...payload };
-  if (copy.children.length > 0) {
-    for (let i = 0; i < copy.children.length; i++) {
-      // properties needed: id, name, desc, start, stop, status, children
-      copy.children[i] = {
-        id: "tempID-" + Math.floor(Math.random() * 100000),
-        name: copy.children[i].subtask_name,
-        desc: copy.children[i].subtask_desc,
-        start: copy.children[i].subtask_start_date,
-        end: copy.children[i].subtask_end_date,
-        status: false,
-        children: []
+  let copy = { //format the task
+    name: payload.name,
+    id: "tempID-" + Math.floor(Math.random() * 100000), 
+    status: false,
+    children: []
+  };
+
+  //format the nibbles
+  function findNibble(name){
+    let n = [];
+    let copyN = [...payload.nibbles];
+    if(payload.nibbles.length === 0){
+      return n;
+    }
+    console.log("name: ", name);
+    console.log("nibble array: ", copyN);
+    //use filter array method? 
+    for(let i=0; i< copyN.length; i++){
+      console.log(copyN[i]);
+      if(copyN[i].subtask_name === name){
+        console.log('matched');
+        n.push({
+          id: "tempID-" + Math.floor(Math.random() * 100000),
+          name: copyN[i].nibble_name,
+          status: false,
+          children: []
+        });
       }
+      else{
+        console.log('no match');
+      }
+    }
+    return n;
+  }
+  
+  if(payload.subtasks.length > 0) {
+    //add in the subtasks if exists
+    for(let i = 0; i < payload.subtasks.length; i++) {
+      let n = findNibble(payload.subtasks[i].subtask_name); //find nibble if exists
+      // properties needed: id, name, status, children
+      copy.children.push({
+        id: "tempID-" + Math.floor(Math.random() * 100000),
+        name: payload.subtasks[i].subtask_name,
+        status: false,
+        children: n
+      });
     }
   }
   return copy;
 }
 
-function deleteTask(state, payload) {
-  for (let i = 0; i < state.length; i++) {
-    if (state[i].id === payload.id) {
-      let copy = [...state];
-      copy.splice(state.indexOf(state[i]), 1); //delete the index
-      return copy;
+//Depth first serach => no early break from recursion
+//Delete the specified index
+
+function deleteTask(s, payload) {
+  // console.log("looking for ID: ", payload.id);
+  for (let i = 0; i < s.length; i++) {
+    if (s[i].id === payload.id) {
+      s.splice(s.indexOf(s[i]), 1); //remove
+      return;
     }
-    else if (state[i].children.length > 0) {
-      deleteTask(state[i].children, payload);
+    else if (s[i].children.length > 0) {
+      deleteTask(s[i].children, payload);
     }
   }
 }
 
-function updateTask(state, payload) {
+function updateTask(state, form ) {
+  // console.log("FORM: ", form);
   for (let i = 0; i < state.length; i++) {
-    if (state[i].id === payload.id) {
-      let copy = [...state];
-      copy.splice(state.indexOf(state[i]), 1, payload); //replace the whole object
-      return copy;
+    if (state[i].id === form.id) {
+      console.log(check + "updating found id: ", state[i].id);
+      state.splice(state.indexOf(state[i]), 1, form); //replace the whole object
+      return;
     }
     else if (state[i].children.length > 0) {
-      updateTask(state[i].children, payload);
+      updateTask(state[i].children, form);
     }
   }
 }
